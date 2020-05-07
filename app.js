430ff0f32d18dafd97d5dfb89b0020d11036d6d9
MDROKIBUL371 <56304400+MDROKIBUL371@users.noreply.github.com>
2020-05-07 20:03:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,11 +49,9 @@ app.use('/api/search', searchRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 
-// Serve static assets if in production
+// Serve client static files
 if (process.env.NODE_ENV === 'production') {
-   // Set static folder
    app.use(express.static('client/build'));
-
    app.get('/', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
    });
